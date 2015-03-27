3556b1a9af19f68171f38064f5b9cbde89d68d5b
Matthew Lamb <lamb.matthew@gmail.com>
2015-03-27 17:19:48 -0500

    Changed to use of a StringBuilder in drawPyramid Changed drawPyramid to return the StringBuilder instead of outputting by itself

--- pset1/pset1/Mario.java
+++ pset1/pset1/Mario.java
@@ -5,43 +5,45 @@ import java.lang.Exception;
 
 public class Mario {
 	
-	private static void DrawPyramid(int Height)
+	private static StringBuilder drawPyramid(int height)
 	{
 		//The pyramid will be Height hashes tall
-		int Hashes = 2;
-		while(Hashes < Height + 2)
+		int hashes = 2;
+		StringBuilder sb = new StringBuilder();
+		while(hashes < height + 2)
 	    {
-	        for(int i=0; i <= (Height - Hashes); i++)
+	        for(int i=0; i <= (height - hashes); i++)
 	        {
-	            System.out.print(" ");
+	           sb.append(" ");
 	            // Print the spaces
 	        }
-	        for(int i=0; i < Hashes; i++)
+	        for(int i=0; i < hashes; i++)
 	        {
-	            System.out.print("#");
+	            sb.append("#");
 	            // Print the hashes
 	        }
-	        System.out.print("\n");
-	        Hashes++;
-	     }   
+	        sb.append("\n");
+	        hashes++;
+	     }
+		return sb;
 	}
 
 	public static void main(String[] args) {
 		// Main method for Mario class, responsible for
 		// getting the height of the pyramid then passing control
 		// to BuildPyramid(height);
-		int Height = -1;
+		int height = -1;
 		
 		// Initialize new scanner object to get an int
 		Scanner getInt = new Scanner(System.in);
 		
-		while (Height < 0 || Height > 23)
+		while (height < 0 || height > 23)
 		{			
 			try 
 			{
 				// Let's get an int
 				System.out.print("Height?: ");
-				Height = getInt.nextInt();
+				height = getInt.nextInt();
 			}
 			catch (Exception e)
 			{
@@ -50,7 +52,7 @@ public class Mario {
 				System.out.println("Invalid input, try an integer!");
 				getInt.next();
 			}
-			if (Height < 0 || Height > 23)
+			if (height < 0 || height > 23)
 			{
 				System.out.println("Try a number from 0 to 23");
 			}
@@ -60,7 +62,7 @@ public class Mario {
 		getInt.close();
 		
 		// Draw the pyramid
-		DrawPyramid(Height);
+		System.out.print(drawPyramid(height));
 		
 	}
 
